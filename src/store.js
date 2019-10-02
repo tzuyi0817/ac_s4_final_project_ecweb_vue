f25c15a186e18935bb68a5494117241cc29d0128
Tzuyi Yang <a0988426059@gmail.com>
2019-10-02 21:32:16 +0800

    add front work for vuex

---
+++ src/store.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    currentUser: {
+      id: -1,
+      StoreId: '',
+      role: '',
+      name: '',
+      email: '',
+      address: '',
+      password: '',
+      emailVerf: false,
+      resetPasswordToken: '',
+      resetPasswordExpires: '',
+      image: ''
+    },
+    isAuthenticated: false
+  },
+  mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser
+      }
+      // 將使用者的登入狀態改為 true
+      state.isAuthenticated = true
+    }
+  },
+  actions: {
+
+  }
+})
