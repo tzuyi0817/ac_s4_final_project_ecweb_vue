434f3836efbad6c8326b3eb404794ea9f3fde7a6
Tzuyi Yang <a0988426059@gmail.com>
2020-04-26 01:09:02 +0800

    Change footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,70 +1,90 @@
 <template>
   <footer id="footer">
-    <div class="container mt-4">
-      <p>YOUTUBE| 客服信箱 | 運費說明 | 常見QA | 隱私權及版權聲明</p>
-      <div class="footer-logistics">
-        <p>物流合作</p>
-        <img
-          style="width: 100px;"
-          src="https://www.logolynx.com/images/logolynx/39/39a41016f188293d5de7ea4f854ff873.png"
-        />
-        <img
-          style="width: 100px;"
-          src="http://pic.pimg.tw/senseofvision/7937b7df062642055f33024a445972ca.jpg"
-        />
-      </div>
+    <div class="container">
+      <div class="row">
+        <div class="footer-title offset-md-2 col-md-4">
+          <h6>各項連結</h6>
+          <p>關於我們</p>
+          <p>合作提案</p>
+          <p>媒體版面</p>
+          <p>新手上路 | 常見 Q&amp;A</p>
+          <p>會員約定書</p>
+        </div>
 
-      <div class="footer-social-icon mt-3">
-        <p>關注我們</p>
-        <router-link to>
-          <i class="fab fa-facebook-f"></i>
-        </router-link>
-        <router-link to>
-          <i class="fab fa-twitter"></i>
-        </router-link>
-        <router-link to>
-          <i class="fab fa-github-alt"></i>
-        </router-link>
-        <router-link to>
-          <i class="fab fa-skype"></i>
-        </router-link>
-        <router-link to>
-          <i class="fab fa-linkedin-in"></i>
-        </router-link>
-      </div>
+        <div class="footer-social-icon offset-md-1 col-md-5">
+          <h6>連絡我們</h6>
+          <div class="icon">
+            <i class="fab fa-facebook-square"></i>
+            <p>Facebook</p>
+          </div>
 
-      <div class="footer-content mt-3">
-        <p>&copy; 2019-20 MuseClub | All Rights Reserved.</p>
+          <div class="icon">
+            <i class="fas fa-envelope"></i>
+            <p>xxx@museclub.tw</p>
+          </div>
+
+          <div class="icon">
+            <i class="fab fa-line"></i>
+            <p>@xxxxxx</p>
+          </div>
+
+          <div class="icon">
+            <i class="fab fa-facebook-messenger"></i>
+            <p>私訊FB專頁</p>
+          </div>
+
+          <div class="icon">
+            <i class="fas fa-phone-square-alt"></i>
+            <p>(02)xxxx-xxxx (平日9:00~18:00)</p>
+          </div>
+        </div>
+
+        <div class="footer-logistics col-12">
+          <p>物流合作</p>
+          <img
+            style="width: 100px;"
+            src="https://www.logolynx.com/images/logolynx/39/39a41016f188293d5de7ea4f854ff873.png"
+          />
+          &nbsp;
+          <img
+            style="width: 100px;"
+            src="http://pic.pimg.tw/senseofvision/7937b7df062642055f33024a445972ca.jpg"
+          />
+        </div>
+
+        <div class="footer-content mt-3 col-12">
+          <p>&copy; 2019-2020 MuseClub | All Rights Reserved.</p>
+        </div>
       </div>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
-@mixin respond-between($lower, $upper, $font-size) {
-  @media screen and (min-width: $lower) and (max-width: $upper) {
-    font-size: $font-size;
-  }
-}
-
-@mixin respond-and($upper, $font-size) {
-  @media screen and (max-width: $upper) {
-    font-size: $font-size;
-  }
-}
-
-p {
-  color: #0085a5;
-  @include respond-between(960px, 1100px, 15px);
-  @include respond-between(768px, 960px, 10px);
-  @include respond-and(768px, 15px);
-}
-
 footer {
   padding: 20px 0;
   text-align: center;
-  background: linear-gradient(-40deg, #d2f0f5, #abe3ed) !important;
-  color: #0085a5;
+  background-color: #f7f8f8;
+  color: rgb(112, 109, 109);
+}
+
+h6 {
+  padding: 20px 0;
+  text-align: left;
+}
+
+p {
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    color: #0085a5;
+    cursor: pointer;
+  }
+}
+
+.footer-title {
+  p {
+    text-align: left;
+  }
 }
 
 .footer-content {
@@ -74,14 +94,13 @@ footer {
 }
 
 .footer-social-icon {
-  a {
-    i {
-      color: #0085a5;
-      display: inline-block;
-      font-size: 20px;
-      line-height: 20px;
-      margin-right: 15px;
-    }
+  i {
+    color: #0085a5;
+    margin-right: 10px;
+    margin-top: 4px;
+  }
+  .icon {
+    display: flex;
   }
 }
 </style>
